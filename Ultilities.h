acc531a722714173e0f520dc6d29eccea63f6e60
TeamSkibidi <skibiditolet553@gmail.com>
2025-04-20 20:29:10 +0700

    Create Ultilities.h

---
+++ Ultilities.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include "Movement.h"
+
+// Check win
+bool checkwin(int** BoardGame, int sizeBoard);
+// Check lost
+bool checklose(int** BoardGame, int sizeBoard);

b70a60de33b9f15b5a416971e56806a16d878674
TeamSkibidi <skibiditolet553@gmail.com>
2025-04-20 20:30:21 +0700

    Create Game_Functions.h

---
+++ Game_Functions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+// Stores predefined function commands for game controls
+struct FunctionGame {
+	string UNDO = "UNDO";
+	string REDO = "REDO";
+	string HOME = "HOME";
+	string REFRESH = "REFRESH";
+	string BACK = "BACK";
+
+};
